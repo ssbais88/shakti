d5de8cda06851ed10d2b2d1e56c3106406b0f16b
sgsohel <sgsohel@gmail.com>
2019-03-21 22:41:15 +0530

    update sohel

---
+++ 100HEL/PHP/com.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+		$a=111;
+		$b=222;
+		$c=333;
+		$d=444;
+		if ($a>$b) {
+			if ($a>$c) {
+				if ($a>$d) {
+					echo "A is Greater";
+				}
+				else{
+					echo "D is Greater";
+				}
+			}
+			else{
+				echo "C is Greater";
+			}
+		}
+		else{
+			if ($b>$c) {
+				if ($b>$d) {
+					echo "B is Greater";
+				}
+				else{
+					echo "D is Greater";
+				}
+			}
+			else{
+				echo "C is Greater";
+			}
+		}
+	?>
+
+</body>
+</html>
