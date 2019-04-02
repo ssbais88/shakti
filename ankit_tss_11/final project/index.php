a1d9bf4785e0ac8ffa7a8a659734fd9dc9d101e0
ankitpurohit2042 <ankitpurohit2042>
2019-04-02 14:58:50 +0530

    rewrite abdul project

--- ankit_tss_11/final project/index.php
+++ ankit_tss_11/final project/index.php
@@ -1,78 +1,7 @@
-<html>
-<head>
-	<title>Pay Cart Shopping</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
-	<script type="text/javascript" src="js/jquery.js"></script>
-
-	<script type="text/javascript" src="js/bootstrap.bundle.js"></script>
-</head>
-<body>
-	<nav class="navbar bg-dark navbar-dark navbar-expand-sm pl-5 p-0 m-0" style="background-color: #15965A;">
-		<button class="navbar-toggler" data-toggle="collapse" data-target="#top-bar">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="top-bar">
-			<center>
-				<ul class="navbar-nav pl-5 ml-3">
-					<li class="nav-link">
-						<a href="#" class="nav-link">Home</a>					
-					</li>
-					<li class="nav-link">
-						<a href="#" class="nav-link">About Us</a>					
-					</li>
-					<li class="nav-link">
-						<a href="#" class="nav-link">Contact</a>					
-					</li>
-					<li class="nav-link">
-						<a href="#" class="nav-link">Help</a>					
-					</li>
-					<li class="nav-link">
-						<a href="#" class="nav-link">FAQ</a>					
-					</li>
-				</ul>
-			</center>
-		</div>
-	</nav>
-	<div class="container-fluid sticky-top mb-0 pb-0"style="background-color: #000;">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-2 col-sm-12" ><img src="image/1.jpg"></div>
-				<form class="form-inline-xl col-md-5 my-2 pt-1 ">
-					<div class="input-group-prepend ">
-					<input type="text" class="form-control" placeholder="Search for product brands and many more....">
-						<span class="input-group-text bg-light bt-md"><i class="fa fa-search" aria-hidden="true"></i></span>
-
-					</div>
-				</form>
-				<ul class="nav mt-2 mx-0 offset-md-1">
-					<li class="nav-item">
-						<a href="#" class="nav-link text-light">Login & Signup</a>
-					</li>
-					<li class="nav-item">
-						<a href="#" class="nav-link text-light"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart</a>
-					</li>
-					<li class="nav-item">
-						<a href="#" class="nav-link text-light"><i class="fa fa-user" aria-hidden="true"></i> My Account</a>
-					</li>
-					
-				</ul>
-				<ul class="nav nav mt-2">
-					<li class="nav-item dropdown">
-						<a href="#" class="nav-link active dropdown-toggle text-light " data-toggle="dropdown">More</a>
-						<div class="dropdown-menu bg-secondary  texrt-dark">
-							<a href="#" class="dropdown-item text-dark font-weight-bold">Notification Prefrences</a>
-							<a href="#" class="dropdown-item text-dark font-weight-bold">Sell on Pay Cart</a>
-							<a href="#" class="dropdown-item text-dark font-weight-bold">24x7 Customer care</a>
-							<a href="#" class="dropdown-item text-dark font-weight-bold">Advertise</a>
-						</div>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
+<?php
+include("common-files/header.php");
+include("common-files/slider.php")
+?>
 	<div class="container-fluid  pt-5" style="background-color: #fff;">
 		<div class="row">
 			<div class="col-md-12 ">
@@ -298,7 +227,6 @@
 			</div>
 		</div>
 	</div>
-	<div class="jumbotron bg-dark mb-0 mt-2 img-fluid max-width=100% max-height-auto" style="background-image: url('image/2.jpeg');background-size: 100%;height: auto;"><center><h1>Welcome To Pay Cart Shopping</h1></center></div>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 bg-warning mt-3 mb-3">
