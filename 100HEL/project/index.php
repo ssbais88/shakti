760545857e74ffc5d57980d7e03743ab405a86fd
sgsohel <sgsohel@gmail.com>
2019-04-03 00:37:02 +0530

    work on images, login and sigup page

---
+++ 100HEL/project/index.php
@@ -0,0 +1,97 @@
+<?php
+include("header.php")
+?>
+	<div class="bg-secondary my-2">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h3 class="bg-secondary text-light my-2 p-1">Categories</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="card mb-4">
+					<div class="card-header">
+						<span class="font-weight-bold">Baby Toys</span>
+					</div>
+					<div class="card-body">
+						<center><img src="image/toy.png" style="height: 150px; width: 250px;"></center>
+					</div>
+					<div class="card-footer">
+						<a href="#" class="btn btn-success btn-sm">More</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card mb-4">
+					<div class="card-header">
+						<span class="font-weight-bold">Newborn Store</span>
+					</div>
+					<div class="card-body">
+						<center><img src="image/2.png" style="height: 150px; width: 250px;"></center>
+					</div>
+					<div class="card-footer">
+						<a href="#" class="btn btn-success btn-sm">More</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card mb-4">
+					<div class="card-header">
+						<span class="font-weight-bold">Diapers & Wipes</span>
+					</div>
+					<div class="card-body">
+						<center><img src="image/diaper.png" style="height: 150px; width: 250px;"></center>
+					</div>
+					<div class="card-footer">
+						<a href="#" class="btn btn-success btn-sm">More</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card mb-4">
+					<div class="card-header">
+						<span class="font-weight-bold">Baby Care</span>
+					</div>
+					<div class="card-body">
+						<center><img src="image/babycare.png" style="height: 150px; width: 250px;"></center>
+					</div>
+					<div class="card-footer">
+						<a href="#" class="btn btn-success btn-sm">More</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card mb-4">
+					<div class="card-header">
+						<span class="font-weight-bold">Nursing & Feeding</span>
+					</div>
+					<div class="card-body">
+						<center><img src="image/feeding.png" style="height: 150px; width: 250px;"></center>
+					</div>
+					<div class="card-footer">
+						<a href="#" class="btn btn-success btn-sm">More</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card mb-4">
+					<div class="card-header">
+						<span class="font-weight-bold">Baby Clothing</span>
+					</div>
+					<div class="card-body">
+						<center><img src="image/11.png" style="height: 150px; width: 250px;"></center>
+					</div>
+					<div class="card-footer">
+						<a href="#" class="btn btn-success btn-sm">More</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php
+include("footer.php")
+?>
