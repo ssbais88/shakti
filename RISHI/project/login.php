a32d3640bd7fa5ab83f8b832cfed2c352f530e7f
Rishu99077 <rishu99077@gmail.com>
2019-04-10 13:56:33 +0530

    login auth

--- RISHI/project/login.php
+++ RISHI/project/login.php
@@ -1,5 +1,6 @@
 <?php
-include("header.php")
+include("header.php");
+session_start();
 ?>
 <div class="row">
 	<div class="col-md-12">
@@ -7,6 +8,7 @@ include("header.php")
 			<div class="card-header">
 				<h3>LOGIN</h3>
 			</div>
+			<form action="auth.php" method="post">
 			<div class="card-body">
 				<div class="form-group">
 					<label for="full">Full Name</label>
@@ -27,15 +29,23 @@ include("header.php")
 								<i class="fa fa-lock"></i>
 							</span>
 						</div>
-						<input type="text" name="pass" class="form-control" placeholder="password">
+						<input type="password" name="pass" class="form-control" placeholder="password">
 					</div>
 				</div>
 			</div>
 			<div class="card-footer">
-				<button class="btn btn-success">
-					<a href="#" class="text-dark">LOGIN</a>
-				</button>
+				<input type="submit" value="login" class="btn btn-success">
 			</div>
+			<p class="text-danger text-center">
+				<?php
+				if(isset($_SESSION['msg']))
+				{
+					echo $_SESSION['msg'];
+					unset($_SESSION['msg']);
+				}
+				?>
+			</p>
+		</form>
 		</div>
 	</div>
 </div>
