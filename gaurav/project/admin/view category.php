23e4295bea4f4533ddd72db680f053eef6c37556
gaurav8630 <1992singh.sks@gmail.com>
2019-04-16 14:38:26 +0530

    project uploaded

---
+++ gaurav/project/admin/view category.php
@@ -0,0 +1,39 @@
+<?php
+include("db.php");
+include("dashboard.php");
+
+$query = "select * from admincategory";
+$result = mysqli_query($con,$query);
+
+?>
+<br>
+<br>
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 offset-md-2">
+			<table class="table table-dark table-bordered table-hover">
+				<tr>
+					<th>S.no</th>
+					<th>Category name</th>
+					<th>Edit</th>
+					<th>Delete</th>
+				</tr>
+				<?php
+				while($data = mysqli_fetch_assoc($result))
+				{
+				?>
+				<tr>
+					<td><?php echo $data['id'] ?></td>
+					<td><?php echo $data['cate_name'] ?></td>
+					<td><a href="#" class="btn btn-info">Edit</a></td>
+					<td><a href="delete_cate.php?cate=<?php echo $data['id']?>" class="btn btn-danger">Delete</a></td>
+				</tr>
+
+
+				<?php
+			}
+			?>
+			</table>
+		</div>
+	</div>
+</div>
