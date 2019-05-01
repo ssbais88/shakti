05ad9ca339c9806d4c2bbcbcea67e235949517d6
jamesjoel <james.joelj@gmail.com>
2019-05-01 13:06:41 +0530

    validation in jquery

---
+++ project/js/val.js
@@ -0,0 +1,161 @@
+$(document).ready(function(){
+	$("#submit").click(function(){
+		var a = $("#full_name").val();
+		var b = $("#profile_pic").val();
+		var c = $("#username").val();
+		var d = $("#pass").val();
+		var e = $("#re_pass").val();
+		var f = $("#add").val();
+		var g = $("#contact").val();
+		var h = $("#city").val();
+
+
+		var x = $("#male").is(":checked");
+		var y = $("#female").is(":checked");
+
+
+		var check = true;
+
+		if(a=="")
+		{
+			check=false;
+			$("#full_name_msg").html("Insert Your Full Name");
+		}
+		else
+		{
+			$("#full_name_msg").html("");
+		}
+		if(b=="")
+		{
+			check=false;
+			$("#profile_pic_msg").html("Select Your Image");
+		}
+		else
+		{
+			$("#profile_pic_msg").html("");
+		}
+		if(c=="")
+		{
+			check=false;
+			$("#username_msg").html("Insert Your Username/Email");
+		}
+		else
+		{
+			
+			$("#username_msg").html("");
+			var reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			if(reg.test(c)==false)
+			{
+				check=false;
+				$("#username_msg").html("Insert Correct Email Id");
+			}
+			else
+			{
+				$("#username_msg").html("");
+
+			}
+
+
+
+
+
+		}
+		if(d=="")
+		{
+			check=false;
+			$("#pass_msg").html("Insert your Password");
+		}
+		else
+		{
+			$("#pass_msg").html("");
+
+		}
+		if(e=="")
+		{
+			check=false;
+			$("#re_pass_msg").html("Insert Re-Password");
+		}
+		else
+		{
+			
+			$("#re_pass_msg").html("");
+			if(d != e)
+			{
+				check=false;
+				$("#re_pass_msg").html("Password and Re-Password not Same");
+			}
+			else
+			{
+				$("#re_pass_msg").html("");
+			}
+
+
+
+
+		}
+		if(f=="")
+		{
+			check=false;
+			$("#add_msg").html("Insert Your Address");
+		}
+		else
+		{
+			$("#add_msg").html("");
+
+		}
+		if(g=="")
+		{
+			check=false;
+			$("#contact_msg").html("Insert Your Contact Number");
+		}
+		else
+		{
+			
+			$("#contact_msg").html("");
+			if(isNaN(g)==true)
+			{
+				check=false;
+				$("#contact_msg").html("Insert Numbers Only");
+
+			}
+			else
+			{
+				$("#contact_msg").html("");
+				if(g.length != 10)
+				{
+					check=false;
+					$("#contact_msg").html("Insert 10 Digits");
+				}
+				else
+				{
+					
+					$("#contact_msg").html("");
+				}
+
+			}
+		}
+		if(h=="Select")
+		{
+			check=false;
+			$("#city_msg").html("Select Your City");
+		}
+		else
+		{
+			$("#city_msg").html("");
+		}
+
+		if(x==false && y==false)
+		{
+			check=false;
+			$("#gender_msg").html("Select Gender");
+		}
+		else
+		{
+			$("#gender_msg").html("");
+		}
+
+
+		// var a = document.getELementById("full_name").value;
+		return check;
+	});	
+});
--- project/signup.php
+++ project/signup.php
@@ -2,6 +2,8 @@
 include("db.php");
 include("header.php");
 ?>
+<script type="text/javascript" src="js/val.js"></script>
+
 		<div class="col-md-9 border p-4">
 			<div class="row">
 				<div class="col-md-12">
@@ -18,40 +20,48 @@ include("header.php");
 							<form action="save.php" method="post" enctype="multipart/form-data">
 								<div class="form-group">
 									<label>Full Name</label>
-									<input type="Text" name="full_name" placeholder="Full Name" class="form-control">
+									<input id="full_name" type="Text" name="full_name" placeholder="Full Name" class="form-control">
+									<p class="text-danger" id="full_name_msg"></p>
 								</div>
 								<div class="form-group">
 									<label>Profile Pic</label>
-									<input type="file" name="image" placeholder="Full Name" class="form-control">
+									<input type="file" id="profile_pic" name="image" placeholder="Full Name" class="form-control">
+									<p class="text-danger" id="profile_pic_msg"></p>
 								</div>
 								<div class="form-group">
 									<label>Username/Email</label>
-									<input type="Text" name="username" placeholder="Username" class="form-control">
+									<input type="Text" id="username" name="username" placeholder="Username" class="form-control">
+									<p class="text-danger" id="username_msg"></p>
 								</div>
 								<div class="form-group">
 									<label>Password</label>
-									<input type="password" name="pass" placeholder="Password" class="form-control">
+									<input type="password" id="pass" name="pass" placeholder="Password" class="form-control">
+									<p class="text-danger" id="pass_msg"></p>
 								</div>
 								<div class="form-group">
 									<label>Re-Password</label>
-									<input type="password" placeholder="Re-Password" class="form-control">
+									<input type="password" id="re_pass" placeholder="Re-Password" class="form-control">
+									<p class="text-danger" id="re_pass_msg"></p>
 								</div>
 								<div class="form-group">
 									<label>Gender</label>
-									<input value="male" type="radio" name="gender" />Male
-									<input value="female" type="radio" name="gender" />Female
+									<input value="male" type="radio" id="male" name="gender" />Male
+									<input value="female" type="radio" id="female" name="gender" />Female
+									<p class="text-danger" id="gender_msg"></p>
 								</div>
 								<div class="form-group">
 									<label>Address</label>
-									<textarea class="form-control" name="add" placeholder="Address"></textarea>
+									<textarea class="form-control" id="add" name="add" placeholder="Address"></textarea>
+									<p class="text-danger" id="add_msg"></p>
 								</div>
 								<div class="form-group">
 									<label>Contact</label>
-									<input type="text" name="contact" placeholder="Contact" class="form-control">
+									<input type="text" name="contact" id="contact" placeholder="Contact" class="form-control">
+									<p class="text-danger" id="contact_msg"></p>
 								</div>
 								<div class="form-group">
 									<label>City</label>
-									<select class="form-control" name="city">
+									<select class="form-control" name="city" id="city">
 										<option>Select</option>
 										<option>Indore</option>
 										<option>Bhopal</option>
@@ -59,12 +69,13 @@ include("header.php");
 										<option>Pune</option>
 										<option>Select</option>
 									</select>
+									<p class="text-danger" id="city_msg"></p>
 								</div>
 								
 
 						</div>
 						<div class="card-footer">
-							<input type="submit" value="Signup" class="btn btn-success">
+							<input type="submit" id="submit" value="Signup" class="btn btn-success">
 						</div>
 						</form>
 					</div>
