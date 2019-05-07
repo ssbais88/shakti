0551aed1f66cf17eab8e091704e92d7229bcf0bd
bodana007 <meghasahu954@gmail.com>
2019-05-07 15:24:24 +0530

    ci

---
+++ Dinesh/myci/application/views/demo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript">
+		function demo()
+		{
+			var a=document.getElementById("box").innerHTML;
+			alert(a);
+		}
+	</script>
+</head>
+<body>
+      <h1 id="box"></h1>
+</body>
+</html>
---
+++ myci/application/views/contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		body{
+			background-color: #3652AB;
+		}
+	</style>
+</head>
+<body>
+<h1>This is Contact Page</h1>
+</body>
+</html>
