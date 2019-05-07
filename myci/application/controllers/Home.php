5502046c0f177a5b9fdd3fa1be7f7407debc6464
jamesjoel <james.joelj@gmail.com>
2019-05-07 13:09:39 +0530

    CI day 1

---
+++ myci/application/controllers/Home.php
@@ -0,0 +1,22 @@
+<?php
+
+class Home extends CI_Controller{
+
+
+	function about()
+	{
+		$this->load->view("about");
+	}
+	function contact()
+	{
+		$this->load->view("contact");
+	}
+	function help()
+	{
+		$this->load->view("help");
+	}
+
+
+}
+
+?>
