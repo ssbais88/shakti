c3cba935f6f75cc70d46ba5953b85aad638d9a07
iamdipen <dipendtrasinghpawar@gmail.com>
2019-05-09 16:43:48 +0530

    ci new file

--- myci/application/controllers/Home.php
+++ myci/application/controllers/Home.php
@@ -3,10 +3,10 @@
 class Home extends CI_Controller{
 
 
-<<<<<<< HEAD
+
 	function about()
 	{
-=======
+
 	function index()
 	{
 		$a = "rohit";
@@ -21,23 +21,22 @@ class Home extends CI_Controller{
 	function about()
 	{
 		$this->load->helper("url"); 
->>>>>>> 72c74d3e160286d982d75ba4237a39095739b1fb
+
 		$this->load->view("about");
 	}
 	function contact()
 	{
-<<<<<<< HEAD
-=======
+
+
 		$this->load->helper("url"); 
->>>>>>> 72c74d3e160286d982d75ba4237a39095739b1fb
+
 		$this->load->view("contact");
 	}
 	function help()
 	{
-<<<<<<< HEAD
-=======
+
+
 		$this->load->helper("url"); 
->>>>>>> 72c74d3e160286d982d75ba4237a39095739b1fb
 		$this->load->view("help");
 	}
 
