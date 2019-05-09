14415b7e03582777a23f6f9ea439c02d69854b87
iamdipen <dipendtrasinghpawar@gmail.com>
2019-05-09 18:38:42 +0530

    update by sachin

--- myci/application/controllers/Home.php
+++ myci/application/controllers/Home.php
@@ -1,47 +1,39 @@
 <?php
-
-class Home extends CI_Controller
-{
-
-
-
-	function about()
+class Home extends CI_Controller{
+	function __construct()
 	{
-
+		parent::__construct();
+		$this->load->helper("url");
+	}
 	function index()
 	{
-		$a = "rohit";
-		$b = "nilesh";
-		$demo = array("x"=>"shubh");
-
-		$pagedata = array("name1"=>$a, "name2"=>$b, "data"=>$demo);
-		$this->load->helper("url"); 
-		$this->load->view("home", $pagedata);
+		$pagedata = array("title"=>"Home Page", "pagename"=>"home");
+		$this->load->view("layout", $pagedata);
 	}
-
 	function about()
 	{
-		$this->load->helper("url"); 
-
-		$this->load->view("about");
+		$pagedata = array("title"=>"About Page", "pagename"=>"about");
+		$this->load->view("layout", $pagedata);
 	}
 	function contact()
 	{
-
-
-		$this->load->helper("url"); 
-
-		$this->load->view("contact");
+		$pagedata = array("title"=>"Contact Page", "pagename"=>"contact");
+		$this->load->view("layout", $pagedata);
 	}
 	function help()
 	{
-
-
-		$this->load->helper("url"); 
-		$this->load->view("help");
+		$pagedata = array("title"=>"Help Page", "pagename"=>"help");
+		$this->load->view("layout", $pagedata);
 	}
-
-
+	function login()
+	{
+		$pagedata = array("title"=>"Login Page", "pagename"=>"login");
+		$this->load->view("layout", $pagedata);
+	}
+	function signup()
+	{
+		$pagedata = array("title"=>"Signup Page", "pagename"=>"signup");
+		$this->load->view("layout", $pagedata);
+	}
 }
-
 ?>
