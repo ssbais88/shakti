ea974e4ddcdc8e0a5957cdbfd480e171fbfbbbdb
jamesjoel <james.joelj@gmail.com>
2019-05-10 12:52:42 +0530

    validation day 1

--- myci/application/controllers/Home.php
+++ myci/application/controllers/Home.php
@@ -1,19 +1,15 @@
 <?php
-
 class Home extends CI_Controller{
 	function __construct()
 	{
 		parent::__construct();
 		$this->load->helper("url");
 	}
-
-
 	function index()
 	{
 		$pagedata = array("title"=>"Home Page", "pagename"=>"home");
 		$this->load->view("layout", $pagedata);
 	}
-
 	function about()
 	{
 		$pagedata = array("title"=>"About Page", "pagename"=>"about");
@@ -34,14 +30,36 @@ class Home extends CI_Controller{
 		$pagedata = array("title"=>"Login Page", "pagename"=>"login");
 		$this->load->view("layout", $pagedata);
 	}
+
+
+
+
 	function signup()
 	{
-		$pagedata = array("title"=>"Signup Page", "pagename"=>"signup");
-		$this->load->view("layout", $pagedata);
+		$this->load->library("form_validation");
+		$this->form_validation->set_rules("full_name", "Full Name", "required");
+		$this->form_validation->set_rules("username", "Username/Email", "required");
+		$this->form_validation->set_rules("pass", "Password", "required");
+		$this->form_validation->set_rules("re_pass", "Re-Password", "required");
+		$this->form_validation->set_rules("add", "Address", "required");
+		$this->form_validation->set_rules("city", "City", "required");
+		$this->form_validation->set_rules("gender", "Gender", "required");
+		$this->form_validation->set_rules("contact", "Contact", "required");
+
+
+		if($this->form_validation->run()==false)
+		{
+			$pagedata = array("title"=>"Signup Page", "pagename"=>"signup");
+			$this->load->view("layout", $pagedata);
+		}
+		else
+		{
+			echo "yes";
+		}
+
+
+
+		
 	}
-
-
 }
-
 ?>
-
